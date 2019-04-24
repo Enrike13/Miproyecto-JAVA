6bf4cd0775aab2a4cfed660336258323748986b9
Enrike13 <enrikefen20@gmail.com>
2019-04-24 11:08:10 -0500

    Ejercicios practicos de JAVA

---
+++ CalculoCuadrado.java
@@ -0,0 +1,14 @@
+/*EJERCICO N°6
+Hacer un programa que calcule el cuadrado de una suma
+(a+b)^2 = a^2+b^2+2ab
+*/
+package consola;
+
+public class CalculoCuadrado {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}

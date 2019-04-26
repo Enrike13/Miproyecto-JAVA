e84b128f8143a5eac3486cdb24ecc9c4c1f90ae5
Enrique Encarnacion <enrikefen20@gmail.com>
2019-04-26 17:08:56 -0500

    Agregando condicionales

---
+++ ejercicios/EJ01_SumaNotas.java
@@ -0,0 +1,23 @@
+/* EJERCICIO N°1
+Hacer un programa que calcule e imprima la suma de tres calificaciones
+*/
+package ejercicios;
+
+import java.util.Scanner;
+
+public class EJ01_SumaNotas {
+    public static void main(String[] args) {
+        float nota1, nota2, nota3, suma, prom;
+        
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Digite 3 calificaciones: ");
+        nota1 = entrada.nextFloat();
+        nota2 = entrada.nextFloat();
+        nota3 = entrada.nextFloat();
+        
+        suma = nota1+nota2+nota3;
+        prom = suma/3;
+        System.out.println("\nLa suma de notas es: "+suma);
+        System.out.println("El promedio es: "+prom);
+    }
+}
---
+++ ejercicios/EJ07_NumHoras.java
@@ -0,0 +1,30 @@
+/*EJERCICIO N°7
+Construir un programa que, dado el número total de horas, devuelve el número de
+semanas, días y horas equivalentes. Por ejemplo, dado un total de 1000 horas
+debe mostrar 5 semanas, 6 días y 16 horas.
+ */
+package ejercicios;
+
+import java.util.Scanner;
+
+public class EJ07_NumHoras {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int horasTotales, semanas, dias, horas;
+        
+        System.out.print("Digite el número de horas: ");
+        horasTotales = entrada.nextInt();
+        
+        semanas = horasTotales /(24 * 7);
+        dias = horasTotales%168 / 24;    //168 horas a la semana
+        horas = horasTotales%24;
+        
+        System.out.println("\nEL EQUIVALENTE ES: ");
+        System.out.println("Semanas: "+semanas);
+        System.out.println("Días: "+dias);
+        System.out.println("Horas: "+horas);
+        
+    }
+    
+}
